34ec0c457c7820f8cf097f8bed9490c41c0c4691
Nicholas Law <nick@nicholaslaw.com.au>
2016-10-22 20:38:53 +0000

    remove unnecessary paraent instantiator

--- src/Exception/InvalidFruitTypeExceptiopn.php
+++ src/Exception/InvalidFruitTypeExceptiopn.php
@@ -3,9 +3,7 @@ namespace Fruit\Exception;
 
 class InvalidFruitTypeExceptiopn extends FruitException {
 
-    public function __construct($message) {
-        parent::__construct($message, 0);
-    }
+    public function __construct($message, $code = 0) {}
 
     public function __toString() {
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
